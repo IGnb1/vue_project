4fb456ad2c4f8eb65089a45a8c93df1624edb65f
闪电金刚之葫芦娃 <1402141265@qq.com>
2019-08-25 18:52:39 +0800

    修改用户信息和添加新用户

--- src/api/user_index.js
+++ src/api/user_index.js
@@ -6,3 +6,20 @@ export const getAllUsers = (params) => {
     params
   })
 }
+// 编辑用户信息
+export const edituser = (data) => {
+  return axios({
+    url: `users/${data.id}`,
+    method: 'put',
+    data
+  })
+}
+
+// 添加用户
+export const add = (data) => {
+  return axios({
+    url: 'users',
+    method: 'post',
+    data
+  })
+}
--- src/views/users.vue
+++ src/views/users.vue
@@ -16,7 +16,7 @@
       >
         <el-button slot="append" icon="el-icon-search" @click="init"></el-button>
       </el-input>
-      <el-button type="success">添加用户</el-button>
+      <el-button type="success" @click="adduser">添加用户</el-button>
     </div>
     <!-- 表格 -->
     <el-table :data="userList" border style="width: 100%;margin-top:15px">
@@ -26,13 +26,18 @@
       <el-table-column prop="mobile" label="电话"></el-table-column>
       <el-table-column label="用户状态">
         <template slot-scope="scope">
-            <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          <el-switch v-model="scope.row.mg_state" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
         </template>
       </el-table-column>
-      <el-table-column label="操作"  width="400">
+      <el-table-column label="操作" width="400">
         <template slot-scope="scope">
           <el-tooltip class="item" effect="dark" content="编辑" placement="top">
-            <el-button type="primary" plain icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)"></el-button>
+            <el-button
+              type="primary"
+              plain
+              icon="el-icon-edit"
+              @click="handleEdit(scope.$index, scope.row)"
+            ></el-button>
           </el-tooltip>
           <el-tooltip class="item" effect="dark" content="分配角色" placement="top">
             <el-button type="success" plain icon="el-icon-share"></el-button>
@@ -51,15 +56,71 @@
       :page-sizes="[1, 2, 3, 4]"
       :page-size="userobj.pagesize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
+      :total="total"
+    ></el-pagination>
+    <!-- 编辑 -->
+    <el-dialog title="编辑信息" :visible.sync="editDialogFormVisible">
+      <el-form :model="editForm" :rules="rules" ref="editForm">
+        <el-form-item label="姓名" :label-width="'80px'" prop="username">
+          <el-input v-model="editForm.username" autocomplete="off" disabled style="width:80px"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="'80px'" prop="email">
+          <el-input v-model="editForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" :label-width="'80px'" prop="mobile">
+          <el-input v-model="editForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editUser">确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 添加用户 -->
+    <el-dialog title="添加用户" :visible.sync="addDialogFormVisible">
+      <el-form :model="addForm" :rules="rules" ref="addForm">
+        <el-form-item label="用户名" :label-width="'80px'" prop="username">
+          <el-input v-model="addForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" :label-width="'80px'" prop="password">
+          <el-input v-model="addForm.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="'80px'" prop="email">
+          <el-input v-model="addForm.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" :label-width="'80px'" prop="mobile">
+          <el-input v-model="addForm.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getAllUsers } from '@/api/user_index.js'
+import { getAllUsers, edituser, add } from '@/api/user_index.js'
 export default {
   data () {
     return {
+      // 添加
+      addDialogFormVisible: false,
+      addForm: {
+        username: '',
+        email: '',
+        mobile: '',
+        password: ''
+      },
+      // 编辑
+      editDialogFormVisible: false,
+      editForm: {
+        username: '',
+        email: '',
+        mobile: '',
+        id: ''
+      },
+      // -------
       total: 0,
       userkey: '',
       statu: true,
@@ -71,24 +132,66 @@ export default {
         pagenum: 1,
         // 每页显示条数
         pagesize: 4
+      },
+      rules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            required: true,
+            message: '请输入邮箱',
+            trigger: 'blur'
+          },
+          {
+            // 添加正则表达式
+            pattern: /\w+[@]\w+[.]\w+/,
+            message: '请输入合法的电子邮箱',
+            trigger: 'blur'
+          }
+        ],
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机号',
+            trigger: 'blur'
+          },
+          {
+            // 添加正则表达式
+            pattern: /^1\d{10}$/,
+            message: '请输入合法的手机号',
+            trigger: 'blur'
+          }
+        ]
       }
     }
   },
   methods: {
     handleSizeChange (val) {
-    //   console.log(`每页 ${val} 条`)
+      //   console.log(`每页 ${val} 条`)
       this.userobj.pagesize = val
       this.init()
     },
     handleCurrentChange (val) {
-    //   console.log(`当前页: ${val}`)
+      //   console.log(`当前页: ${val}`)
       this.userobj.pagenum = val
       this.init()
     },
     init () {
       getAllUsers(this.userobj)
         .then(res => {
-        //   console.log(res)
+          //   console.log(res)
           if (res.data.meta.status === 200) {
             this.userList = res.data.data.users
             this.total = res.data.data.total
@@ -102,10 +205,69 @@ export default {
         })
     },
     handleEdit (index, row) {
-      console.log(index, row)
+      // console.log(indexx, row);
+      this.editDialogFormVisible = true
+      this.editForm.username = row.username
+      this.editForm.email = row.email
+      this.editForm.mobile = row.mobile
+      this.editForm.id = row.id
     },
     handleDelete (index, row) {
-      console.log(index, row)
+      // console.log(index, row);
+    },
+    // 编辑用户
+    editUser () {
+      // console.log(this.editForm)
+      this.$refs.editForm.validate((valid) => {
+        if (valid) {
+          // alert('submit!')
+          edituser(this.editForm)
+            .then(res => {
+              // console.log(res)
+              if (res.data.meta.status === 200) {
+                this.$message.success(res.data.meta.msg)
+                this.init()
+                this.editDialogFormVisible = false
+              }
+            })
+            .catch(() => {
+              // console.log(err)
+              this.$message.warning('用户编辑失败')
+            })
+        } else {
+          // console.log('error submit!!')
+          this.$message.error('请输入合法的信息')
+          return false
+        }
+      })
+    },
+    // 添加用户
+    adduser () {
+      this.addDialogFormVisible = true
+    },
+    addUser () {
+      this.$refs.addForm.validate((valid) => {
+        if (valid) {
+          // alert('submit!')
+          add(this.addForm)
+            .then(res => {
+              // console.log(res)
+              if (res.data.meta.status === 201) {
+                this.addDialogFormVisible = false
+                this.init()
+                this.$message.success(res.data.meta.msg)
+                this.$refs.addForm.resetFields()
+              }
+            })
+            .catch(() => {
+              this.$message.warning('添加用户失败')
+            })
+        } else {
+          // console.log('error submit!!')
+          this.$message.error('请输入合法的信息')
+          return false
+        }
+      })
     }
   },
   mounted () {
